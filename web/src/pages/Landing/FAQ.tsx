75468a3d70ef5681f4597bf38be17c0e14ada87d
Dhara Panchal <dhara_panchal@csu.fullerton.edu>
2025-12-21 21:23:58 -0800

    feat: add FAQ section

--- web/src/pages/Landing/FAQ.tsx
+++ web/src/pages/Landing/FAQ.tsx
@@ -2,49 +2,48 @@ import { useState } from "react";
 import Anchor from "../../assets/LandingPage/FAQ/Anchor.svg";
 
 type FAQType = {
-  question: string
-  answer: string 
-}
+  question: string;
+  answer: string;
+};
 
 const faqList: FAQType[] = [
   {
     question: "beans",
-    answer: "beans answer"
+    answer: "beans answer",
   },
   {
     question: "beans1",
-    answer: "beans answer1"
+    answer: "beans answer1",
   },
   {
     question: "beans2",
-    answer: "beans answer2"
+    answer: "beans answer2",
   },
   {
     question: "beans3",
-    answer: "beans answer3"
+    answer: "beans answer3",
   },
   {
     question: "beans4",
-    answer: "beans answer4"
+    answer: "beans answer4",
   },
   {
     question: "beans5",
-    answer: "beans answer5"
+    answer: "beans answer5",
   },
   {
     question: "beans6",
-    answer: "beans answer6"
+    answer: "beans answer6",
   },
   {
     question: "beans7",
-    answer: "beans answer7"
+    answer: "beans answer7",
   },
   {
     question: "beans8",
-    answer: "beans answer8"
+    answer: "beans answer8",
   },
-]
-
+];
 
 export function FAQ() {
   // this keeps track of which FAQ is currently open, store the index of the open FAQ and is nothing is open, it will be null
@@ -53,9 +52,14 @@ export function FAQ() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-slate-100 w-full relative mt-5 gap-12">
       <h2 className="justify-start text-sky-200 text-9xl font-normal font-nemo lowercase"> FAQ</h2>
-      <div className= "flex flex-col gap-6 w-full max-w-400 px-4">
+      <div className="flex flex-col gap-6 w-full max-w-400 px-4">
         {faqList.map((faqListItem, index) => (
-          <FAQDropdown question={faqListItem.question} answer={faqListItem.answer} idx={index} openedFAQ={openedFAQ} setOpenedFAQ={setOpenedFAQ}
+          <FAQDropdown
+            question={faqListItem.question}
+            answer={faqListItem.answer}
+            idx={index}
+            openedFAQ={openedFAQ}
+            setOpenedFAQ={setOpenedFAQ}
           />
         ))}
       </div>
@@ -84,21 +88,23 @@ function FAQDropdown({
   return (
     <button
       onClick={handleClick}
-      className="flex justify-between items-start gap-4 py-4 px-5 sm:px-8 w-full rounded-2xl sm:rounded-[30px] border-2 sm:border-[5px] border-white text-left ursor-pointer hover:bg-white/10 transition-colors duration-200"
+      className="flex justify-between items-start gap-4 py-4 px-5 sm:px-8 w-full rounded-2xl sm:rounded-[30px] border-2 sm:border-[5px] border-white text-left cursor-pointer hover:bg-white/10 transition-colors duration-200"
     >
       <div className="flex flex-col max-w-[calc(100%-40px)]">
         {/* displays the question */}
-        
+
         <p className="flex justify-start text-teal-500 text-xl sm:text-3xl md:text-5xl font-nemo wrap-break-word">
           {question}
         </p>
 
         {/* will only display the answer if the FAQ's index matches the openedFAQ index */}
         {openedFAQ === idx && (
-          <p className="text-base sm:text-xl md:text-4xl font-bagel wrap-break-word text-left">{answer}</p>
+          <p className="text-base sm:text-xl md:text-4xl font-bagel wrap-break-word text-left">
+            {answer}
+          </p>
         )}
       </div>
-      
+
       {/* displays the anchor  */}
       <div className="w-10">
         <img src={Anchor} alt="Anchor" className="w-full h-auto" />
