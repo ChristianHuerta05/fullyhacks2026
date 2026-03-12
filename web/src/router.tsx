8f3df76ccc0defad11f0b26306af56f7caeee96f
Christian Huerta <christianhuerta309@gmail.com>
2026-03-11 17:17:28 -0700

    feat: add custom 404/error page with ocean theme

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -4,13 +4,15 @@ import { PortalPage } from "./pages/Portal/PortalPage";
 import { AdminPage } from "./pages/Admin/AdminPage";
 import { ApplyPage } from "./pages/Apply/ApplyPage";
 import { ApplicationPage } from "./pages/Application/ApplicationPage";
+import { ErrorPage } from "./pages/Error/ErrorPage";
 
 const router = createBrowserRouter([
-  { path: "/", element: <LandingPage /> },
-  { path: "/profile", element: <PortalPage /> },
-  { path: "/admin", element: <AdminPage /> },
-  { path: "/apply", element: <ApplyPage /> },
-  { path: "/application", element: <ApplicationPage /> },
+  { path: "/", element: <LandingPage />, errorElement: <ErrorPage /> },
+  { path: "/profile", element: <PortalPage />, errorElement: <ErrorPage /> },
+  { path: "/admin", element: <AdminPage />, errorElement: <ErrorPage /> },
+  { path: "/apply", element: <ApplyPage />, errorElement: <ErrorPage /> },
+  { path: "/application", element: <ApplicationPage />, errorElement: <ErrorPage /> },
+  { path: "*", element: <ErrorPage /> },
 ]);
 
 export function AppRouter() {
