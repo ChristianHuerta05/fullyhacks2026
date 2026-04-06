e64d416de86e486452bb4e1dcd894db2cd06393a
bebopkenny <kennygarcia15@csu.fullerton.edu>
2026-04-05 17:15:25 -0700

    fix: changed date and made app responsive

--- web/src/pages/Portal/Fullypacks.tsx
+++ web/src/pages/Portal/Fullypacks.tsx
@@ -116,7 +116,7 @@ export function Fullypacks() {
         </p>
       </div>
 
-      <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 sm:gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 sm:gap-4">
         {FULLYPACKS.map((pack) => (
           <FullypackCard
             key={pack.name}
